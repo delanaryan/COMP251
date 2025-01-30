5f5dd3e7d87f0c7d4b7ece8f7f22eecc31462613
Delana <118754893+delanaryan@users.noreply.github.com>
2025-01-30 11:50:08 -0500

    bugfix disjointsetsA

--- A1/Q2/A1/Q2/A1/Q2/DisjointSetsA.java
+++ A1/Q2/A1/Q2/A1/Q2/DisjointSetsA.java
@@ -19,7 +19,7 @@ public class DisjointSetsA {
 
     /* contructor: creates a partition of n elements. */
     /* Each element is in a separate disjoint set */
-    void DisjointSetsA(int n) {
+    public DisjointSetsA(int n) {
         if (n>0) {
             par = new int[n];
             rank = new int[n];
@@ -70,41 +70,57 @@ public class DisjointSetsA {
     /* merge sets containing elements i and j */
     public int union(int i, int j) {
         /* Fill this method (The statement return 0 is here only to compile) */
-        int rep;
-        
-        if (find(i) > find(j)){
-            rep = par[find(j)];
-            par[find(i)] = rep;
+        int repI = find(i);
+        int repJ = find(j);
+
+        if (repI == repJ) return repI; // Already in the same set
+
+        if (rank[repI] > rank[repJ]) {
+            par[repJ] = repI;
+            return repI;
+        } else if (rank[repI] < rank[repJ]) {
+            par[repI] = repJ;
+            return repJ;
         } else {
-            rep = par[find(i)];
-            par[find(j)] = rep;
+            par[repJ] = repI;
+            rank[repI]++;
+            return repI;
         }
-        return rep;
     }
 
     public static void main(String[] args) {
-
+        long startTime = System.nanoTime();
+    
         DisjointSetsA myset = new DisjointSetsA(6);
         System.out.println(myset);
+    
         System.out.println("-> Union 2 and 3");
-        myset.union(2,3);
+        myset.union(2, 3);
         System.out.println(myset);
+    
         System.out.println("-> Union 2 and 3");
-        myset.union(2,3);
+        myset.union(2, 3);
         System.out.println(myset);
+    
         System.out.println("-> Union 2 and 1");
-        myset.union(2,1);
+        myset.union(2, 1);
         System.out.println(myset);
+    
         System.out.println("-> Union 4 and 5");
-        myset.union(4,5);
+        myset.union(4, 5);
         System.out.println(myset);
+    
         System.out.println("-> Union 3 and 1");
-        myset.union(3,1);
+        myset.union(3, 1);
         System.out.println(myset);
+    
         System.out.println("-> Union 2 and 4");
-        myset.union(2,4);
+        myset.union(2, 4);
         System.out.println(myset);
-
+    
+        long endTime = System.nanoTime();
+        double elapsedTime = (endTime - startTime) / 1_000_000.0;
+        System.out.println("Total execution time: " + elapsedTime + " ms");
     }
 
 }
