e10767cc860660a0607b039fa0c809249bf9027e
Delana <118754893+delanaryan@users.noreply.github.com>
2025-03-01 19:36:55 -0500

    A2Q4 Finished

--- A2/Assignment.java
+++ A2/Assignment.java
@@ -20,8 +20,10 @@ class Assignment implements Comparator<Assignment>{
 	@Override
 	public int compare(Assignment a1, Assignment a2) {
 		// TODO Implement this
-		
-		
-		return 0;
+		if (a1.weight != a2.weight) {
+			return Integer.compare(a2.weight, a1.weight);
+		} else {
+			return Integer.compare(a1.deadline, a2.deadline);
+		}
 	}
 }
